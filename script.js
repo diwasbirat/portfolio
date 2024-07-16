1f6184cef492fddec731790a2a44a674c3de718e
Diwas poudel <72566444+diwasbirat@users.noreply.github.com>
2024-07-16 23:22:40 +0545

    Update script.js

--- script.js
+++ script.js
@@ -34,19 +34,19 @@ $(document).ready(function(){
     });
 
     // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", " Web Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing", {
+    //     strings: ["YouTuber", " Web Developer", "Blogger", "Designer", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
